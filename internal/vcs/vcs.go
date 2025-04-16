b30f03d91cdbdc9fef61f4a35961eecb63adc9d3
denis-k2 <dxxx999@gmail.com>
2025-04-16 17:05:08 +0700

    fix(version): display release tag instead of commit hash

--- internal/vcs/vcs.go
+++ internal/vcs/vcs.go
@@ -1,37 +1,15 @@
 package vcs
 
 import (
-	"fmt"
 	"runtime/debug"
 )
 
-// generates a version number for application based on the commit hash
+// returns application version set during build from release tag
 func Version() string {
-	var (
-		time     string
-		revision string
-		modified bool
-	)
-
 	bi, ok := debug.ReadBuildInfo()
 	if ok {
-		for _, s := range bi.Settings {
-			switch s.Key {
-			case "vcs.time":
-				time = s.Value
-			case "vcs.revision":
-				revision = s.Value
-			case "vcs.modified":
-				if s.Value == "true" {
-					modified = true
-				}
-			}
-		}
+		return bi.Main.Version
 	}
 
-	if modified {
-		return fmt.Sprintf("%s-%s-dirty", time, revision)
-	}
-
-	return fmt.Sprintf("%s-%s", time, revision)
+	return ""
 }
